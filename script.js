19cd94d30d170bec19944eb61047e391bcba5fab
sokpheak <136253781+sokpheak12@users.noreply.github.com>
2024-06-05 08:05:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+///Hooray No JS :)
+
+/*
+
+========================================================
+A Loving Car with pure CSS,  Inspired Dribbble shot for UXDots.
+
+https://dribbble.com/shots/2869958-Loving-car
+========================================================
+
+*/
